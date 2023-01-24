9127c3e3a5cdd082af913d511cbcfb8b6c12cb3a
rnr1721 <admin@ehukr.com>
2023-01-24 21:35:45 +0200

    +PSR7 request and response (implementation from Slim PHP framework) +small refactoring

--- Controller/Web/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace le7\Controller\Web;
-
-class IndexController extends \le7\Controller\ControllerWeb {
-    public function indexAction() {
-        echo 'index';
-    }
-}
--- Controller/Web/TestController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace le7\Controller\Web;
-
-class TestController extends \le7\Controller\ControllerWebSmarty {
-    public function indexAction() {
-        echo 'test';
-    }
-}
